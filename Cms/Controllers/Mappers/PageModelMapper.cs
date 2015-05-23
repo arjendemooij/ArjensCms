aafc149721b6bb49882133949e823a2f551624af
Arjen de Mooij <arjendemooij@gmail.com>
2015-05-23 22:02:50 +0200

    Map to viewmodel using automapper

---
+++ Cms/Controllers/Mappers/PageModelMapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Cms.Data;
+using Cms.Models;
+
+namespace Cms.Controllers.Mappers
+{
+    public class PageModelMapper
+    {
+        public PageModel MapToPageModel(Page page, PageModel model)
+        {
+            if(model == null)
+                model = new PageModel();
+
+            MapperHelper.RequireMap<Page, PageModel>();
+
+            model = Mapper.Map(page, model);         
+
+            return model;
+        }
+    }
+}
