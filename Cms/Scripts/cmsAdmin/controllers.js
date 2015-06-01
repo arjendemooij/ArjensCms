159c46654a549af21b1e7d1148f32d136111a62c
Arjen de Mooij <arjendemooij@gmail.com>
2015-06-01 21:59:28 +0200

    make controller, app and service entity agnostic

--- Cms/Scripts/cmsAdmin/controllers.js
+++ Cms/Scripts/cmsAdmin/controllers.js
@@ -1,26 +1,74 @@
 ﻿angular.module('cmsAdmin.controllers', [])
-.controller('PageListController', ['$scope', 'Page', function ($scope, pageService) {
+.controller('ListController', ['$scope', '$state', 'Service', function ($scope, $state, service) {
+
     
-    $scope.delete = function (page) {
-        $scope.page = page;
-        page.$delete(function() {
-            $scope.pages = pageService.query();
+    $scope.isSearching = false;
+    if (!$scope.PageNumber) {        
+        $scope.PageNumber = 0;
+    }
+    $scope.PageSize = 3;
+
+    $scope.delete = function (item) {
+        $scope.item = item;
+        service.delete({ Id: item.Id }, function () {
+            reloadData();
         });
-        
     };
 
-    $scope.pages = pageService.query();
+    $scope.edit = function (item) {
+       
+        $state.go('edit', { Id: item.Id });
+
+    };
+
+    $scope.search = function (pageNumber) {
+      
+        $scope.PageNumber = pageNumber;
+        reloadData();
+    };
+
+    reloadData();
+
+
+
+    function reloadData() {
+        
+        $scope.items = service.query({
+            pageNumber: $scope.PageNumber,
+            pageSize: $scope.PageSize
+        }, function (result) {
+            $scope.items = result.Data;
+            $scope.PageCount = result.PageCount;
+        });
+    }
+
 }])
 
-.controller('PageAddController', ['$scope', '$routeParams', '$state', 'Page', function ($scope, $routeParams,$state, page) {
-    $scope.page = new page();
-    
-    console.log("saving");
-    
-    $scope.addPage = function () {
-        $scope.page.$save(function () {
-            $state.go('pages');
+.controller('AddController', ['$scope', '$state', 'Service', function ($scope, $state, service) {
+
+    $scope.item = new service();
+
+    $scope.save = function () {
+        $scope.item.$save(function () {
+            $state.go('list');
         });
     };
 
-}]) 
+}])
+
+.controller('EditController', ['$scope', '$state', '$stateParams', 'Service', function ($scope, $state, $stateParams, service) {
+
+    $scope.item = service.query({ Id: $stateParams.Id }, function (value) {
+        console.log(value);
+    });
+
+
+    $scope.save = function () {
+        $scope.item.$save(function () {
+            $state.go('list');
+        });
+    };
+
+
+}])
+
