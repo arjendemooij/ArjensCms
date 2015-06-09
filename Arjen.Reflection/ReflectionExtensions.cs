cd25ca9fdf12435cb1b5e3e6f428b1b973caf692
Arjen de Mooij <arjendemooij@gmail.com>
2015-06-09 20:53:32 +0200

    work in progress

---
+++ Arjen.Reflection/ReflectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arjen.Reflection
+{
+    public static class ReflectionExtensions
+    {
+        public static bool HasInterface<TInterface>(this Type type)
+        {
+            return type.GetInterfaces().Contains(typeof(TInterface));
+        }
+
+        public static bool IsEnumerableType(this Type type)
+        {
+            return type.GetInterfaces().Contains(typeof (IEnumerable));
+        }
+
+        public static Type GetEnumerableItemType(this Type type)
+        {
+            return type.GetGenericArguments().First();
+        }
+    }
+}
---
+++ Cms/Areas/Admin/Models/PageAuthorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Cms.Areas.Admin.Models
+{
+    public class PageAuthorModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
